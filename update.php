42583a53b84eef2c6b91006f93d0f129645cccbe
manuel-rahm <manuel.rahm@bbz-sh.ch>
2022-01-25 10:23:56 +0100

    b.4

--- update.php
+++ update.php
@@ -49,35 +49,46 @@ if($_POST['inputTask'] == NULL && $_POST['inputRITM'] == NULL) {
 
 
 
-                $updateci = "UPDATE tblci
-                SET fldCI = (:ci) WHERE NOT EXISTS (SELECT 1 FROM tblci AS b WHERE b.fldCI = (:ci))";
-                $stmt1 = $connection->prepare($sql1);
+                
+                
 
                 $updaterequester = "UPDATE tblrequester
                 SET fldRequester = (:requester) WHERE NOT EXISTS (SELECT 1 FROM tblrequester AS b WHERE b.fldRequester = (:requester))";
-                $stmt2 = $connection->prepare($sql2);
+                $stmtupdaterequester = $connection->prepare($updaterequester);
+                
                 
                 $updatelocation = "UPDATE tbllocation
                 SET fldLocation = (:loc) WHERE NOT EXISTS (SELECT 1 FROM tbllocation AS b WHERE b.fldLocation = (:loc))";
-                $stmt3 = $connection->prepare($sql3);
+                $stmtupdatelocation = $connection->prepare($updatelocation);
+                
 
                 $updatedata = "UPDATE tbltasks SET fldTaskNr = (:tasknr), fldRITMNr = (:ritmnr), fldCHGNr = (:chgnr), fkGxP = (:gxp), fkStatus = (:stat), fldDescription = (:descr), fkResponsible = (:responsible)
                 WHERE fldTaskNr = (:tasknr)";
-                $stmt = $connection->prepare($sql);
+                $stmtupdatedata = $connection->prepare($updatedata);
+                
 
                 $updatefkci = "UPDATE tbltasks SET fkCI = (SELECT pkCI FROM tblCI AS b WHERE b.fldCI = (:ci)) WHERE fldTaskNr = (:tasknr)";
-                $stmt4 = $connection->prepare($sql4);
-                $stmt4->execute($ci);
+                $stmtupdatefkci = $connection->prepare($updatefkci);
+                
 
                 $updatefkrequester = "UPDATE tbltasks SET fkRequester = (SELECT pkRequester FROM tblrequester AS b WHERE b.fldRequester = (:requester)) WHERE fldTaskNr = (:tasknr)";
-                $stmt5 = $connection->prepare($sql5);
-                $stmt5->execute($requester);
+                $stmtupdatefkrequester = $connection->prepare($updatefkrequester);
+                $stmtupdatefkrequester->execute($requester);
 
                 $updatefklocation = "UPDATE tbltasks SET fkLocation = (SELECT pkLocation FROM tbllocation AS b WHERE b.fldLocation = (:loc)) WHERE fldTaskNr = (:tasknr)";
-                $stmt6 = $connection->prepare($sql6);
-                $stmt6->execute($location);
+                $stmtupdatefklocation = $connection->prepare($updatefklocation);
+                
 
-                if ($stmt1->execute($ci) && $stmt2->execute($requester) ){
+                if ($stmtinsertci->execute($ci) && $stmtinsertrequester->execute($requester) &&  $stmtinsertlocation->execute($location) && $stmtinsertdata->execute($data)){
+                        $updateci = "UPDATE tblci
+                                SET fldCI = (:ci) WHERE NOT EXISTS (SELECT 1 FROM tblci AS b WHERE b.fldCI = (:ci))";
+                        $stmtupdateci = $connection->prepare($updateci);
+                        $stmtupdateci->execute($ci);
+                        $stmtupdaterequester->execute($requester);
+                        $stmtupdatelocation->execute($location);
+                        $stmtupdatedata->execute($data);
+                        $stmtupdatefkci->execute($ci);
+                        $stmtupdatefklocation->execute($location);
                 echo '<p class="success">New record created successfully!</p><style>p {font-weight:bold; font-size:20px;}</style>';
                 echo '<script type="text/javascript">setTimeout(function () {
                         window.location.href = "index.php";}, 2000);</script>';
